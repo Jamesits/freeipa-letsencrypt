bedad493d49a90d8deb2ad4ad5d728c7bf47df6f
James Swineson <github@public.swineson.me>
2017-11-21 15:43:59 +0800

    Do you really need verbose?

--- setup-le.sh
+++ setup-le.sh
@@ -6,9 +6,9 @@ source config.sh
 apt-get install letsencrypt -y
 
 ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
-ipa-certupdate -v
+ipa-certupdate
 
 ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
-ipa-certupdate -v
+ipa-certupdate
 
 "$(dirname "$0")/renew-le.sh" "--first-time"
